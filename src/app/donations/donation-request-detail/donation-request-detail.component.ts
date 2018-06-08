1ae924dc640531c3ecbc590ba3745d68e4bd9fa5
Juan Pablo Orlando <juanpabloorlando9498@gmail.com>
2018-06-08 19:38:57 -0300

    CustomModalDialog

--- src/app/donations/donation-request-detail/donation-request-detail.component.ts
+++ src/app/donations/donation-request-detail/donation-request-detail.component.ts
@@ -1,11 +1,8 @@
-// ACA VA LA LOGICA de ver el detalle de una req elegida por la ONG
-// Se van a mostrar todos las Responses asociadas
-
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { DonationRequest, DonationRequestApi, DonationResponseApi, DonationResponse } from '../../shared/sdk';
 import { MatTableDataSource } from '@angular/material';
-
 import { Router, ActivatedRoute } from '@angular/router';
+import { CustomModalService } from '../../shared/utils/custom-modal/custom-modal.service';
 
 @Component({
     selector: 'app-donation-request-detail',
@@ -20,18 +17,19 @@ export class DonationRequestDetailComponent implements OnInit {
     constructor(
         private router: Router,
         private route: ActivatedRoute,
-        private donationResponseApi: DonationResponseApi
+        private donationResponseApi: DonationResponseApi,
+        public customModal: CustomModalService
     ) {
         this.getParams()
             .subscribe(params => {
                 const requestId = params['idRequest'];
                 this.donationResponseApi.find({
-                    include: [ 'donationRequest', 'donner', 'donnerReview' ],
+                    include: [ {donationRequest: ['product']}, 'donner', 'donnerReview'],
                     where: {
                         donationRequestId: requestId
                     }
                 }).subscribe(responses => {
-                    console.log({responses});
+                    console.log({ responses });
                     this.dataSource.data = <DonationResponse[]>responses;
                 });
             });
@@ -41,9 +39,14 @@ export class DonationRequestDetailComponent implements OnInit {
         return this.route.queryParams;
     }
 
-    public confirmDelivery(requestId) {
-        console.log(requestId);
-        // this.donationResponseApi.donationArrival(requestId);
+    public confirmDelivery(response) {
+        let data = {
+            title: 'Confirmar entrega',
+            description: `¿Confirma que le ha llegado ${response.amount} 
+            unidades de ${response.donationRequest.description}?`,
+        }
+        this.customModal.openDialog(data);
+        // this.donationResponseApi.donationArrival(response.id);
     }
 
     ngOnInit() {
@@ -54,6 +57,6 @@ export class DonationRequestDetailComponent implements OnInit {
         filterValue = filterValue.trim(); // Remove whitespace
         filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
         this.dataSource.filter = filterValue;
-      }
+    }
 
 }
