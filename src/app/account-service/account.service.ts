523bf585c96e3a8cfcb55bb410d3a554db47364e
Juan Pablo Orlando <juanpabloorlando9498@gmail.com>
2018-06-26 10:54:07 -0300

    wrong credentials alerts added

--- src/app/account-service/account.service.ts
+++ src/app/account-service/account.service.ts
@@ -28,10 +28,13 @@ export class AccountService {
   // Built-in LoopBack Authentication and Typings like Account and TokenInterface
   public signinUser(user: Donner): void {
     this.donnerApi.login(user).subscribe((token: SDKToken) => {
-        this.router.navigate(['/catalog']);
-    //   this.auth.setToken(token); ya lo hace el metodo login
+      this.router.navigate(['/catalog']);
+      //   this.auth.setToken(token); ya lo hace el metodo login
       this.auth.setRememberMe(true); // Creo que tampoco hace falta
       this.auth.save(); // Creo que tampoco hace falta
+    }, (err) => {
+      console.error(err);
+      alert("Credenciales incorrectos!");
     });
   }
   public logoutUser(): void {
@@ -41,9 +44,12 @@ export class AccountService {
     });
   }
   public signinOrganization(organization: Organization): void {
-    this.organizationApi.login(organization).subscribe((token: SDKToken) => { 
-        this.router.navigate(['/catalog']);
-        // We must do something to know that is an organization ... 
+    this.organizationApi.login(organization).subscribe((token: SDKToken) => {
+      this.router.navigate(['/catalog']);
+      // We must do something to know that is an organization ... 
+    }, (err) => {
+      console.error(err);
+      alert("Credenciales incorrectos!");
     });
   }
 
